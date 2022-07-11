6dee98a8fa108eebad9d4594d94870a968c63bbb
Rajesh <35864342+hirajesh@users.noreply.github.com>
2022-07-11 17:44:04 +0530

    Webdriver IO
    Initial commit

---
+++ test/pageobjects/signin.page.js
@@ -0,0 +1,46 @@
+class signin {
+
+
+  get lansignIn() {
+    return $("#account-actions-signin");
+  }
+  get lanpassWord() {
+    return $('//*[@id="standalone-j_password"]');
+  }
+  get lanuserName() {
+    return $('//*[@id="standalone-j_username"]');
+  }
+  get lanlogin() {
+    return $("#standalone-signin-form-submit");
+  }
+  get lancloseverify() {
+    return $('//*[@id="device-id-close"]');
+  }
+  get lanbasket() {
+    return $('//*[text()="Basket"]');
+  }
+
+  async usersignin(user, pass) {
+//    await browser.setTimeout({ pageLoad: 50000 });
+    await this.lanuserName.setValue(user);
+    await this.lanpassWord.setValue(pass);
+    await this.lanlogin.click();
+  }
+  async land_signIN() {
+    await this.lansignIn.click();
+    browser.saveScreenshot("./signinpage.png");
+    await browser.setTimeout({ pageLoad: 50000 });
+  }
+  async landopenurl() {
+    await browser.maximizeWindow();
+   // await browser.url("https://lms.com:L@digital@uat1.homecentre.com/ae/en/");
+    await browser.url("https://www.homecentre.com/ae/en/");
+    browser.saveScreenshot("./homepage.png");
+  }
+  async land_close_mobileverification() {
+    await this.lancloseverify.click();
+  }
+
+}
+
+module.exports = new signin();
---
+++ test/specs/azure.js
@@ -0,0 +1,12 @@
+const signin=require('../pageobjects/signin.page')
+
+describe('This test will execute sign in ',()=>{
+    it('Signin test',async()=>{
+
+       await signin.landopenurl();
+       await signin.land_signIN();
+       await signin.usersignin('landmarkeod2021@gmail.com','123456');
+       await signin.land_close_mobileverification();
+
+    })
+})
